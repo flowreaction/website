ac7abea4f7b61eaadb7d6b1aaaaaac9936b936da
Bopp, Florian <florian.bopp@haw-hamburg.de>
2022-04-03 21:16:10 +0200

    replaced a tags with nuxtlinks for optimized routing and active link styling

--- components/CustomHeader.vue
+++ components/CustomHeader.vue
@@ -1,21 +1,22 @@
 <template>
   <header
-    class="fixed h-16 w-screen border-b-4 border-gray-500 bg-white dark:border-cyan-300 dark:bg-slate-800 dark:text-cyan-100"
+    class="fixed h-16 w-screen border-b-4 border-gray-800 bg-white dark:border-cyan-300 dark:bg-slate-800 dark:text-cyan-100"
   >
     <div class="container flex h-full justify-between md:justify-start">
-      <div class="flex h-full items-center">
+      <div class="mr-4 flex h-full items-center">
         <h1 class="font-mono md:text-3xl">Florian Bopp</h1>
         <span
-          class="animate-caret font-mono font-extrabold text-cyan-800 [text-shadow:0px_0px_8px_rgba(0,255,255,0.5)] dark:text-cyan-400 md:text-2xl"
+          class="animate-caret font-mono font-extrabold text-pink-500 [text-shadow:0px_0px_8px_rgba(0,255,255,0.5)] md:text-3xl"
           >_</span
         >
       </div>
       <div class="ml-8 hidden grow items-center gap-2 md:flex">
-        <a
+        <NuxtLink
           :href="link.href"
           v-for="link in links"
-          class="px-2 hover:text-pink-500"
-          >{{ link.text }}</a
+          class="px-2 py-1 hover:text-pink-500"
+          :activeClass="activeClasses"
+          >{{ link.text }}</NuxtLink
         >
       </div>
       <div
@@ -46,6 +47,14 @@ const links = ref([
     href: '/contact',
   },
 ])
+const route = useRoute()
+onUpdated(() => {
+  console.log(route.path)
+})
+
+const activeClasses = computed(() => {
+  return 'bg-pink-600 text-white hover:text-white border-2 border-gray-800 dark:border-cyan-300'
+})
 </script>
 
 <style scoped>
